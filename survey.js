96cf59d8fd73f7fb3d9c4597533033cf0c257724
Hufan <hufany21@gmail.com>
2021-11-03 21:50:55 -0400

    work

--- survey.js
+++ survey.js
@@ -13,8 +13,8 @@ rl.question('What is your names? Nickanmes are also acceptable ', (answer) => {
         rl.question('What\'s your favourite thing to eat for that meal? ', (answer5) => {
           rl.question('Which sport is your absolute favourite? ', (answer6) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! \n \n ', (answer7) => {
-              console.log(`Hey everyone, you can call me ${answer}. On most day's you can catch me on my couch eating ${answer5} for ${answer4}  and listening to ${answer3}. I love ${answer2} and playing or watching 
-              ${answer6}. I'm very into comic books and ${answer7}`)
+              console.log(  `Hey everyone, you can call me ${answer}. On most day's you can catch me on my couch eating ${answer5} for ${answer4}  and listening to ${answer3}. I love ${answer2} and playing or watching 
+              ${answer6}. I'm very into having fun and ${answer7}`)
               rl.close() 
             })   
           })  
